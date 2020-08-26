5f442409f97156ad0c4b4ca261f0b1a416c3c811
artninjackson <artninjackson@gmail.com>
2020-08-25 20:21:10 -0500

    technically first push; have FPS camera, mouse controls, shooting, ammo, a gun ui, animations, etc.

---
+++ Fiori Dungeon/Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    public int ammoAmount = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammoAmount = Random.Range(10, 25);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            PlayerController.instance.currentAmmo += ammoAmount;
+
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Fiori Dungeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Declaring data variables
+    public static PlayerController instance;
+    public Rigidbody2D rb;
+    public float movementSpeed = 5f;
+    private Vector2 movementInput;
+    private Vector2 mouseInput;
+    public float mouseSensitivity = 5f;
+    public Camera cameraView;
+    public GameObject hitImpact;
+    public int currentAmmo = 10;
+    public Animator bowAnimation;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement
+        movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 moveHorizontal = transform.up * -movementInput.x;
+        Vector3 moveVertical = transform.right * movementInput.y;
+        rb.velocity = (moveHorizontal + moveVertical) * movementSpeed;
+
+        //player view rotation and control
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x, 
+            transform.rotation.eulerAngles.y, 
+            transform.rotation.eulerAngles.z - mouseInput.x);
+
+        cameraView.transform.localRotation = Quaternion.Euler(
+            cameraView.transform.localRotation.eulerAngles + 
+            new Vector3(
+            0f, mouseInput.y, 0f));
+
+        //Attacks
+        if(Input.GetMouseButtonDown(0))
+        {
+            //bow
+            if(currentAmmo > 0)
+            {
+                Ray ray = cameraView.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)); ///(.5,.5,0) is the center of the screen
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    //Debug.Log("Looking at " + hit.transform.name);
+                    Instantiate(hitImpact, hit.point, transform.rotation);
+                }
+                else
+                {
+                    //Debug.Log("Nothing to see here, chief.");
+                }
+                currentAmmo--;
+                bowAnimation.SetTrigger("Shoot");
+            }
+            
+            //if sword
+            //TODO
+
+            //if dagger
+            //TODO
+            
+        }
+    }
+}
